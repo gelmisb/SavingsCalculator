020998e9d412ff7ecf5d648d215289b9db2097c8
gelmisb <gelmisb@gmail.com>
2023-04-05 09:49:20 +0100

    Added activity for savings calc

--- app/src/main/java/com/example/savingscalculator/MainActivity.java
+++ app/src/main/java/com/example/savingscalculator/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.savingscalculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button mClickButton1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mClickButton1 = (Button)findViewById(R.id.calc_savings_activity_btn);
+
+        mClickButton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(MainActivity.this, CalcSavingsActivity.class);
+//                myIntent.putExtra("key", value); //Optional parameters
+                MainActivity.this.startActivity(myIntent);
+            }
+        });
     }
+
+
+
+
 }
