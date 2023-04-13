b8bff9ba0d6a80e681dd93b74fd1c7ebd2f6a92f
gelmisb <gelmisb@gmail.com>
2023-04-13 11:24:15 +0100

    Code cleanup - cache now active for second screen

--- app/src/main/java/com/example/savingscalculator/calculatesavings/fragments/a1_IncomeFragment.java
+++ app/src/main/java/com/example/savingscalculator/calculatesavings/fragments/a1_IncomeFragment.java
@@ -9,6 +9,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -17,6 +18,8 @@ import androidx.navigation.fragment.NavHostFragment;
 import com.example.savingscalculator.R;
 import com.example.savingscalculator.databinding.FragmentFirstBinding;
 
+import java.util.ArrayList;
+
 public class a1_IncomeFragment extends Fragment {
 
     private FragmentFirstBinding binding;
@@ -24,8 +27,7 @@ public class a1_IncomeFragment extends Fragment {
     @Override
     public View onCreateView(
             @NonNull LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState
-    ) {
+            Bundle savedInstanceState) {
         binding = FragmentFirstBinding.inflate(inflater, container, false);
         return binding.getRoot();
     }
@@ -35,55 +37,60 @@ public class a1_IncomeFragment extends Fragment {
 
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity().getApplicationContext(),
                 R.array.timeframe_array, android.R.layout.simple_spinner_item);
-
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
-        binding.timeframe.setSelection(adapter.getPosition("Weekly"));
-        binding.timeframe2.setSelection(adapter.getPosition("Weekly"));
-        binding.timeframe3.setSelection(adapter.getPosition("Weekly"));
-        binding.timeframe4.setSelection(adapter.getPosition("Weekly"));
-        binding.timeframe5.setSelection(adapter.getPosition("Weekly"));
-
-        binding.timeframe.setAdapter(adapter);
-        binding.timeframe2.setAdapter(adapter);
-        binding.timeframe3.setAdapter(adapter);
-        binding.timeframe4.setAdapter(adapter);
-        binding.timeframe5.setAdapter(adapter);
-
+        ArrayList<Spinner> spinners = new ArrayList<>();
+        setSpinners(spinners, adapter);
 
         binding.nextBtn2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                SharedPreferences sharedPreferences = getActivity().getSharedPreferences("UserIncome", MODE_PRIVATE);
-                SharedPreferences.Editor editor = sharedPreferences.edit();
-
-                String wagesT = binding.timeframe.getSelectedItem().toString();
-                String pWagesT = binding.timeframe2.getSelectedItem().toString();
-                String cbpT = binding.timeframe3.getSelectedItem().toString();
-                String mainT = binding.timeframe4.getSelectedItem().toString();
-                String otherIT = binding.timeframe5.getSelectedItem().toString();
-
-                Resources res = getResources();
-
-                String wages = res.getString(R.string.wages_social_welfare);
-                String partnerWages = res.getString(R.string.partner_s_wages_social_welfare);
-                String cbp = res.getString(R.string.cbp);
-                String maintenance = res.getString(R.string.income_maintenance);
-                String otherIncome = res.getString(R.string.wages_social_welfare);
-
-                editor.putString(wages, ((binding.editWagesNumber.getText().toString().equals("")) ? "0" : binding.editWagesNumber.getText().toString()) + " " + wagesT);
-                editor.putString(partnerWages, ((binding.editWagesNumber.getText().toString().equals("")) ? "0" : binding.editWagesNumber.getText().toString()) + " " + pWagesT);
-                editor.putString(cbp, ((binding.editWagesNumber.getText().toString().equals("")) ? "0" : binding.editWagesNumber.getText().toString()) + " " + cbpT);
-                editor.putString(maintenance, ((binding.editWagesNumber.getText().toString().equals("")) ? "0" : binding.editWagesNumber.getText().toString()) + " " + mainT);
-                editor.putString(otherIncome, ((binding.editWagesNumber.getText().toString().equals("")) ? "0" : binding.editWagesNumber.getText().toString()) + " " + otherIT);
-
-                editor.apply();
-
+                cacheSelected();
                 NavHostFragment.findNavController(a1_IncomeFragment.this)
                         .navigate(R.id.action_FirstFragment_to_SecondFragment);
             }
         });
     }
+
+    public void setSpinners(ArrayList<Spinner> spinList, ArrayAdapter<CharSequence> adapter){
+        spinList.add(binding.timeframe);
+        spinList.add(binding.timeframe2);
+        spinList.add(binding.timeframe3);
+        spinList.add(binding.timeframe4);
+        spinList.add(binding.timeframe5);
+
+        for (int i = 0; i < spinList.size(); i++) {
+            spinList.get(i).setSelection(adapter.getPosition(getResources().getString(R.string.weekly)));
+            spinList.get(i).setAdapter(adapter);
+        }
+    }
+
+    public void cacheSelected(){
+        SharedPreferences sharedPreferences = getActivity().getSharedPreferences("UserIncome", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        Resources res = getResources();
+
+        String wagesT = binding.timeframe.getSelectedItem().toString();
+        String pWagesT = binding.timeframe2.getSelectedItem().toString();
+        String cbpT = binding.timeframe3.getSelectedItem().toString();
+        String mainT = binding.timeframe4.getSelectedItem().toString();
+        String otherIT = binding.timeframe5.getSelectedItem().toString();
+
+        String wages = res.getString(R.string.wages_social_welfare);
+        String partnerWages = res.getString(R.string.partner_s_wages_social_welfare);
+        String cbp = res.getString(R.string.cbp);
+        String maintenance = res.getString(R.string.income_maintenance);
+        String otherIncome = res.getString(R.string.wages_social_welfare);
+
+        editor.putString(wages, ((binding.editWagesNumber.getText().toString().equals("")) ? "0" : binding.editWagesNumber.getText().toString()) + " " + wagesT);
+        editor.putString(partnerWages, ((binding.editWagesNumber.getText().toString().equals("")) ? "0" : binding.editWagesNumber.getText().toString()) + " " + pWagesT);
+        editor.putString(cbp, ((binding.editWagesNumber.getText().toString().equals("")) ? "0" : binding.editWagesNumber.getText().toString()) + " " + cbpT);
+        editor.putString(maintenance, ((binding.editWagesNumber.getText().toString().equals("")) ? "0" : binding.editWagesNumber.getText().toString()) + " " + mainT);
+        editor.putString(otherIncome, ((binding.editWagesNumber.getText().toString().equals("")) ? "0" : binding.editWagesNumber.getText().toString()) + " " + otherIT);
+
+        editor.apply();
+    }
+
     @Override
     public void onDestroyView() {
         super.onDestroyView();
--- app/src/main/java/com/example/savingscalculator/calculatesavings/fragments/a2_Expenses_LoanFragment.java
+++ app/src/main/java/com/example/savingscalculator/calculatesavings/fragments/a2_Expenses_LoanFragment.java
@@ -3,22 +3,22 @@ package com.example.savingscalculator.calculatesavings.fragments;
 import static android.content.Context.MODE_PRIVATE;
 
 import android.content.SharedPreferences;
+import android.content.res.Resources;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.savingscalculator.R;
-import com.example.savingscalculator.calculatesavings.Income;
 import com.example.savingscalculator.databinding.FragmentSecondBinding;
 
-//import com.example.savingscalculator.calculatesavings.databinding.FragmentSecondBinding;
+import java.util.ArrayList;
 
 public class a2_Expenses_LoanFragment extends Fragment {
 
@@ -27,62 +27,84 @@ public class a2_Expenses_LoanFragment extends Fragment {
     @Override
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState
-    ) {
-
+            Bundle savedInstanceState) {
         binding = FragmentSecondBinding.inflate(inflater, container, false);
         return binding.getRoot();
-
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        float totalIncome = ((Income) getActivity().getApplicationContext()).getTotalIncome();
 
-        Log.i("Total income: ", String.valueOf(totalIncome));
-
-        // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity().getApplicationContext(),
                 R.array.timeframe_array, android.R.layout.simple_spinner_item);
-        // Specify the layout to use when the list of choices appears
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
-
-        binding.spinnerExpenseStep21.setSelection(adapter.getPosition("Weekly"));
-        binding.spinnerExpenseStep22.setSelection(adapter.getPosition("Weekly"));
-        binding.spinnerExpenseStep23.setSelection(adapter.getPosition("Weekly"));
-        binding.spinnerExpenseStep24.setSelection(adapter.getPosition("Weekly"));
-        binding.spinnerExpenseStep25.setSelection(adapter.getPosition("Weekly"));
-        binding.spinnerExpenseStep26.setSelection(adapter.getPosition("Weekly"));
-        binding.spinnerExpenseStep27.setSelection(adapter.getPosition("Weekly"));
-
-        binding.spinnerExpenseStep21.setAdapter(adapter);
-        binding.spinnerExpenseStep22.setAdapter(adapter);
-        binding.spinnerExpenseStep23.setAdapter(adapter);
-        binding.spinnerExpenseStep24.setAdapter(adapter);
-        binding.spinnerExpenseStep25.setAdapter(adapter);
-        binding.spinnerExpenseStep26.setAdapter(adapter);
-        binding.spinnerExpenseStep27.setAdapter(adapter);
+        ArrayList<Spinner> spinners = new ArrayList<>();
+        setSpinners(spinners, adapter);
 
         binding.nextBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                SharedPreferences sharedPreferences = getActivity().getSharedPreferences("UserIncome", MODE_PRIVATE);
-                SharedPreferences.Editor editor = sharedPreferences.edit();
-                editor.putString("wages", (binding.carLoanEdit.getText().toString().equals("")) ? "0" : binding.carLoanEdit.getText().toString());
-                editor.putString("partnerWages", (binding.educationEdit.getText().toString().equals("")) ? "0" : binding.educationEdit.getText().toString());
-                editor.putString("cbp", (binding.homeLoanEdit.getText().toString().equals("")) ? "0" : binding.homeLoanEdit.getText().toString());
-                editor.putString("maintenance", (binding.personalLoanEdit.getText().toString().equals("")) ? "0" : binding.personalLoanEdit.getText().toString());
-                editor.putString("otherIncome", (binding.businessLoanEdit.getText().toString().equals("")) ? "0" : binding.businessLoanEdit.getText().toString());
-                editor.putString("otherIncome", (binding.holidayLoanEdit.getText().toString().equals("")) ? "0" : binding.holidayLoanEdit.getText().toString());
-                editor.putString("otherIncome", (binding.creditCardLoanEdit.getText().toString().equals("")) ? "0" : binding.creditCardLoanEdit.getText().toString());
-                editor.apply();
+                cacheSelected();
 
                 NavHostFragment.findNavController(a2_Expenses_LoanFragment.this)
                         .navigate(R.id.action_SecondFragment_to_ThirdFragment);
             }
         });
+    }
 
+    public void setSpinners (ArrayList<Spinner> spinners, ArrayAdapter<CharSequence> adapter){
+        spinners.add(binding.spinnerExpenseStep21);
+        spinners.add(binding.spinnerExpenseStep22);
+        spinners.add(binding.spinnerExpenseStep23);
+        spinners.add(binding.spinnerExpenseStep24);
+        spinners.add(binding.spinnerExpenseStep25);
+        spinners.add(binding.spinnerExpenseStep26);
+        spinners.add(binding.spinnerExpenseStep27);
+
+        for (int i = 0; i < spinners.size(); i++) {
+            spinners.get(i).setSelection(adapter.getPosition(getResources().getString(R.string.weekly)));
+            spinners.get(i).setAdapter(adapter);
+        }
+    }
+
+    public void cacheSelected(){
+        SharedPreferences sharedPreferences = getActivity().getSharedPreferences("UserIncome", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        Resources res = getResources();
+
+        String carSpin = binding.spinnerExpenseStep21.getSelectedItem().toString();
+        String educationSpin = binding.spinnerExpenseStep22.getSelectedItem().toString();
+        String mortgageSpin = binding.spinnerExpenseStep23.getSelectedItem().toString();
+        String personalSpin = binding.spinnerExpenseStep24.getSelectedItem().toString();
+        String businessSpin = binding.spinnerExpenseStep25.getSelectedItem().toString();
+        String holidaySpin = binding.spinnerExpenseStep26.getSelectedItem().toString();
+        String creditSpin = binding.spinnerExpenseStep27.getSelectedItem().toString();
+
+        String carStr = res.getString(R.string.car_motorbike_loan);
+        String eduStr = res.getString(R.string.education_tuition_loan);
+        String morStr = res.getString(R.string.home_loan);
+        String perStr = res.getString(R.string.personal_loan);
+        String busStr = res.getString(R.string.business_loan);
+        String holStr = res.getString(R.string.holiday_loan);
+        String creStr = res.getString(R.string.credit_card_loan);
+
+        editor.putString(carStr, ((binding.carLoanEdit.getText().toString().equals(""))
+                ? "0" : binding.carLoanEdit.getText().toString()) + " " + carSpin);
+        editor.putString(eduStr, ((binding.educationEdit.getText().toString().equals(""))
+                ? "0" : binding.educationEdit.getText().toString()) + " " + educationSpin);
+        editor.putString(morStr, ((binding.homeLoanEdit.getText().toString().equals(""))
+                ? "0" : binding.homeLoanEdit.getText().toString()) + " " + mortgageSpin);
+        editor.putString(perStr, ((binding.personalLoanEdit.getText().toString().equals(""))
+                ? "0" : binding.personalLoanEdit.getText().toString()) + " " + personalSpin);
+        editor.putString(busStr, ((binding.businessLoanEdit.getText().toString().equals(""))
+                ? "0" : binding.businessLoanEdit.getText().toString()) + " " + businessSpin);
+        editor.putString(holStr, ((binding.holidayLoanEdit.getText().toString().equals(""))
+                ? "0" : binding.holidayLoanEdit.getText().toString()) + " " + holidaySpin);
+        editor.putString(creStr, ((binding.creditCardLoanEdit.getText().toString().equals(""))
+                ? "0" : binding.creditCardLoanEdit.getText().toString()) + " " + creditSpin);
+
+        editor.apply();
     }
 
     @Override
